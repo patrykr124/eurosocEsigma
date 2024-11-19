3318534f98397b8abea5831294085a5f1302d718
Patryk <patrykr124@gmail.com>
2024-11-19 16:06:14 +0100

    15.11

--- components/UI/blue/section/map/Map.tsx
+++ components/UI/blue/section/map/Map.tsx
@@ -1,11 +1,7 @@
 'use client'
-import EncryptButton from "@/components/UI/EncryptButton";
-import LeafletMap from "@/components/UI/LeafletMap";
 import dynamic from 'next/dynamic';
 
 function Map() {
-
-
 
 
     const MapLealfet = dynamic(() => import('../../../LeafletMap'), {
@@ -16,9 +12,10 @@ function Map() {
     return (
         <div className=''>
             <div className="flex flex-col justify-center items-center w-full overflow-hidden ">
-                <iframe id="cyberthreat-map" src="https://livethreatmap.radware.com/" className="w-screen h-[90vh] pointer-events-none border-0 "></iframe>
+                <iframe id="cyberthreat-map" src="https://livethreatmap.radware.com/"
+                        className="w-screen h-[90vh] pointer-events-none border-0 "></iframe>
             </div>
-        </div >
+        </div>
     )
 }
 
