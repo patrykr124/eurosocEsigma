ef5dc451e83c6e5d344e9171a4cf4c91995df039
Patryk <patrykr124@gmail.com>
2024-11-20 14:57:46 +0100

    15.11

--- app/dashboard/Layout.tsx
+++ app/dashboard/Layout.tsx
@@ -1,14 +1,13 @@
-
 export default function RootLayout({
-  children,
-}:{
-  children: React.ReactNode;
+                                       children,
+                                   }: {
+    children: React.ReactNode;
 }) {
-  return (
-    <div className="flex h-screen flex-col">
-      {/* <Header /> */}
-      <main className="flex-1">{children}</main>
-      {/* <Footer /> */}
-    </div>
-  );
+    return (
+        <div className="flex h-screen flex-col">
+
+            <main className="flex-1">{children}</main>
+
+        </div>
+    );
 }
